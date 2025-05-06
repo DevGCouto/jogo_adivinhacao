0f03b70c533be0a1c1e73eecdb8a6b2a08b03e54
Gabriel Couto <gabriel.fcoutor@gmail.com>
2025-05-06 02:04:43 -0300

    Update urls.py

--- jogo_project/urls.py
+++ jogo_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from meu_app.views import jogo_adivinhacao, forcar_migracao  
 
 urlpatterns = [
+    path('', jogo_adivinhacao),
     path('admin/', admin.site.urls),
-    path('', include('meu_app.urls')),
+    path('forcar-migracao/', forcar_migracao), 
 ]
